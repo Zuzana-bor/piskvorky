0b6f51cb2152737ba05003cdb3718efa48814693
Zuzana Borutova <z.kasikova@gmail.com>
2021-04-27 12:41:33 +0200

    Zamezení kluknutí do stejného pole

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ const znak = (event) => {
     event.target.classList.add('board__field--circle');
     hracElm.src = 'obrazky_pravidla/cross.svg';
     kdoJeNaTahu = 'krizek';
+    event.target.disabled = true;
   } else {
     event.target.classList.add('board__field--cross');
     hracElm.src = 'obrazky_pravidla/circle.svg';
     kdoJeNaTahu = 'kolecko';
+    event.target.disabled = true;
   }
 };
 
 const hracElm = document.querySelector('#hrac');
-document.querySelector('.hraciPole').addEventListener('click', znak);
+const polickoElm = document.querySelector('.hraciPole');
+polickoElm.addEventListener('click', znak);
